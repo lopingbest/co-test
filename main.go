742fa051f9aca0a96dffa57aada3fa7b3728a5fd
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-30 21:58:30 +0700

    add isPrimer

--- main.go
+++ main.go
@@ -34,3 +34,16 @@ func factorial(n int) int {
 	}
 	return n * factorial(n-1)
 }
+
+// isPrime
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
