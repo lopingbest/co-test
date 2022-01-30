c738091ea338caf3d96fd72e65037985a97e7d25
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-01-30 21:55:29 +0700

    add fizzbuzz

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, CoPilot!")
 }
+
+//fizzbuzz
+func fizzbuzz(n int) string {
+	if n%15 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	} else {
+		return fmt.Sprintf("%d", n)
+	}
+}
